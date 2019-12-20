283410757af2e0b6b575977b2213b4962d45294f
SavioAndres <savioandres@hotmail.com>
2019-12-20 19:49:52 -0300

    Framework

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Framework PHPSkunk
+ * This content is released under the MIT License (MIT)
+ * 
+ * @package PHPSkunk
+ * @author  Sávio Andres
+ * @license MIT License
+ * @link    https://github.com/SavioAndres/PHPSkunk
+ * @since   Version 0.0.1
+ */
